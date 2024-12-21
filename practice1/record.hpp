5a72e2571f77cc00b2d275a14f076e1f70670f9d
Luigi Eliabe <luigi.eliabe.as@gmail.com>
2024-12-21 17:11:39 +0000

    Add record methods headers

--- practice1/record.hpp
+++ practice1/record.hpp
@@ -1,8 +1,20 @@
-#ifndef RECORD_H
-#define RECORD_H
+#ifndef RECORD_HPP
+#define RECORD_HPP
+
+#include<string>
 
 class Record{
+public:
+    char name[30];
+    int age;
 
+
+    Record();
+    Record(int value, const std::string& str);
+
+    std::string serialize();
+
+    void deserialize(const std::string& buffer);
 };
 
 #endif
