0fba590faae9f718daf778b77d442686fecc6a95
Luigi Eliabe <luigi.eliabe.as@gmail.com>
2024-12-21 17:10:27 +0000

    Add buffer methods headers

--- practice1/buffer.hpp
+++ practice1/buffer.hpp
@@ -1,8 +1,15 @@
-#ifndef BUFFER_H
-#define BUFFER_H
+#ifndef BUFFER_HPP
+#define BUFFER_HPP
+
+#include<string>
+#include"record.hpp"
 
 class Buffer{
+public:
+    Buffer();
 
+    void writeFixedRecord(Record rec, std::ofstream& out);
+    Record readFixedRecord(std::ifstream& in);
 };
 
 #endif
