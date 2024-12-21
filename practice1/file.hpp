1e1bc305fc5e618152c50495cb94b61b12320250
Luigi Eliabe <luigi.eliabe.as@gmail.com>
2024-12-21 17:11:05 +0000

    Add file methods headers

--- practice1/file.hpp
+++ practice1/file.hpp
@@ -1,8 +1,21 @@
-#ifndef FILE_H
-#define FILE_H
+#ifndef FILE_HPP
+#define FILE_HPP
+
+#include<string>
+#include<vector>
+#include"buffer.hpp"
 
 class File{
+public:
+    std::vector<Record> records;
 
+    File();
+
+    std::vector<Record> readRecordCSV(std::string file);
+
+    void addFixedRecord(std::string file);
+
+    std::vector<Record> readRecordFixed(std::string file);
 };
 
 #endif
